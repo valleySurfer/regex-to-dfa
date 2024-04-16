edc8251fd2cebf8897d6ce2e038064d1d4a9a101
revidlluf <86007860+valleySurfer@users.noreply.github.com>
2024-04-16 23:54:48 +0900

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,7 +2,7 @@
 #include "RegexTree.h"
 
 int main() {
-  auto tree = RegexTree("(a|b)*abb");
+  auto tree = RegexTree("(a+b)(b+aa*b)*(a+aa*(a+$))");
   auto dfa = DFA(tree);
   dfa.CreateDotFile("example.gv");
 }
